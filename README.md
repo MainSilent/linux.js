28ab9cab1b123e7f87673bbc2af849b39f2fa1e8
MainSilent <70273681+MainSilent@users.noreply.github.com>
2021-02-14 09:20:26 +0330

    fix library issues

--- README.md
+++ README.md
@@ -49,8 +49,3 @@ sudo chroot ./chroot
 apt install whatever
 exit
 ```
-
-## Contributing
-Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
-
-Please make sure to update tests as appropriate.
--- reqs.sh
+++ reqs.sh
@@ -1,9 +1,9 @@
 if [ -e package.json ]; then
     # check for filesystem
     if [ ! -d filesystem ]; then
-        echo -n "Press enter to download the filesystem ~280MB: "
+        echo -n "Press enter to download the filesystem: "
         read
-        wget "https://github.com/MainSilent/linux.js/releases/download/1.0.0/filesystem.squashfs"
+        wget "https://github.com/MainSilent/linux.js/releases/download/v1.1.0/filesystem.squashfs"
         unsquashfs -d filesystem filesystem.squashfs
         rm -f filesystem.squashfs
     else
